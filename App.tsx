8811fbcdf9aa1c6683e6a11567dc08a739e17528
Shoyeb180 <shoyebmohammad660@gmail.com>
2022-08-14 18:08:28 +0600

    first commit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>My first react native app.Lets goooooo.......</Text>
       <StatusBar style="auto" />
     </View>
   );
