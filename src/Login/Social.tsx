318575510f617d89f7604aa69b861d50f608d9f5
Shoyeb180 <shoyebmohammad660@gmail.com>
2022-08-20 17:02:49 +0600

    google sign in problem solved

--- src/Login/Social.tsx
+++ src/Login/Social.tsx
@@ -1,23 +1,42 @@
 import { View, Text, StyleSheet, Button } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-google-signin/google-signin'
+import * as Google from 'expo-auth-session/providers/google'
+import * as WebBrowser from 'expo-web-browser';
 
-GoogleSignin.configure({
-    webClientId: '962289027598-3f1dddv57d140vuhtnrqn7dkqqt8asdg.apps.googleusercontent.com'
-})
+WebBrowser.maybeCompleteAuthSession();
+
 const Social = () => {
+    const [accessToken,setAccessToken]=useState<any>();
+    const [userInfo,setUserInfo]=useState<any>();
     
+    const [request,response,promptAsync]=Google.useAuthRequest({
+        androidClientId:"962289027598-2tmcskr14d0ooqqs975phao7tsd0fg70.apps.googleusercontent.com",
+        iosClientId: "962289027598-2tmcskr14d0ooqqs975phao7tsd0fg70.apps.googleusercontent.com",
+        expoClientId:"962289027598-2tmcskr14d0ooqqs975phao7tsd0fg70.apps.googleusercontent.com"
+    })
 
+    useEffect(()=>{
+        if(response?.type==='success'){
+            setAccessToken(response.authentication?.accessToken)
+        }
+    },[response])
+
+    const getUserData=async()=>{
+        let userInfoResponse=await fetch('https://www.googleapis.com/userinfo/v2/me',{
+            headers: { Authorization: `Bearer ${accessToken}`}
+        })
+        userInfoResponse.json().then(data=>setUserInfo(data))
+    }
     return (
         <View>
             <View style={styles.button}>
                 <Button
-                    // onPress={() => signInAsync()}
-                    title="Google Sign In"
+                    onPress={() => {promptAsync({showInRecents:true})}}
+                    title="Facebook Sign In"
                 />
-
             </View>
-
+            {/* {error &&
+                <Text>Error: {error.message}</Text>} */}
         </View>
     )
 }
